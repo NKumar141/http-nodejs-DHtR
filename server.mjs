f9a4afb3919c762cc77314e21ea726078204a793
Nikhil <nk110700@gmail.com>
2023-02-25 15:39:15 +0530

    goo

--- server.mjs
+++ server.mjs
@@ -2,7 +2,7 @@ import { createServer } from "http";
 const apidata = require("./data.json");
 
 createServer((req, res) => {
-  res.write("Hello World!");
-  res.send(apidata);
+  res.write(apidata);
+  //res.send(apidata);
   res.end();
 }).listen(process.env.PORT);
